fd87195dc3770bae2eb39c8398eb110983aaca02
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-10-28 12:51:22 +0900

    fix: make commands callable from the entry point.

--- weko_group_cache_db/cli.py
+++ weko_group_cache_db/cli.py
@@ -1,4 +1,4 @@
-from logging import FileHandler, Formatter, getLogger
+# from logging import FileHandler, Formatter, getLogger
 
 import click
 
@@ -6,13 +6,13 @@ from . import config
 from . import messages
 from .utils import set_groups
 
-logger = getLogger(__name__)
-logger.setLevel(config.CLI_LOG_LEVEL)
+# logger = getLogger(__name__)
+# logger.setLevel(config.CLI_LOG_LEVEL)
 
-file_handler = FileHandler(config.CLI_LOG_OUTPUT_PATH)
-formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
+# file_handler = FileHandler(config.CLI_LOG_OUTPUT_PATH)
+# formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# file_handler.setFormatter(formatter)
+# logger.addHandler(file_handler)
 
 @click.group()
 def group():
@@ -29,8 +29,13 @@ def get_groups(fqdn):
     try:
         set_groups(fqdn)
         click.secho(messages.COMMAND_SUCCESS.format(fqdn), fg='green')
-        logger.info(messages.COMMAND_SUCCESS.format(fqdn))
+        # logger.info(messages.COMMAND_SUCCESS.format(fqdn))
     except Exception as ex:
         click.secho(messages.COMMAND_FAILED.format(fqdn, ex), fg='red')
         click.secho(messages.COMMAND_FAILED_DETAIL.format(config.CLI_LOG_OUTPUT_PATH), fg='red')
-        logger.error(ex, exc_info=True)
+        # logger.error(ex, exc_info=True)
+
+def main():
+    """Main function for CLI.
+    """
+    print("weko-group-cache-db CLI")
