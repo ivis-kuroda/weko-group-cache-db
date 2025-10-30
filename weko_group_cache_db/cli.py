f1f3c4dea1327ce17397db3331a1df43f8041e70
ivis-kuroda <tomohiro.kuroda@ivis.co.jp>
2025-10-30 04:52:02 +0000

    refactor: clean up CLI modul.

--- weko_group_cache_db/cli.py
+++ weko_group_cache_db/cli.py
@@ -1,41 +1,30 @@
-# from logging import FileHandler, Formatter, getLogger
+#
+# Copyright (C) 2025 National Institute of Informatics.
+#
+
+"""CLI module for weko-group-cache-db."""
 
 import click
 
-from . import config
-from . import messages
-from .utils import set_groups
+from .logger import logger
+from .utils import fetch_all
 
-# logger = getLogger(__name__)
-# logger.setLevel(config.CLI_LOG_LEVEL)
-
-# file_handler = FileHandler(config.CLI_LOG_OUTPUT_PATH)
-# formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# file_handler.setFormatter(formatter)
-# logger.addHandler(file_handler)
 
 @click.group()
-def group():
-    """Group commands."""
-
-@group.command()
-@click.option('-f', '--fqdn', required=True)
-def get_groups(fqdn):
-    """Get groups from Gakunin API and set to Redis
-
-    Arguments:
-        fqdn(str): fqdn of the target sp
-    """
-    try:
-        set_groups(fqdn)
-        click.secho(messages.COMMAND_SUCCESS.format(fqdn), fg='green')
-        # logger.info(messages.COMMAND_SUCCESS.format(fqdn))
-    except Exception as ex:
-        click.secho(messages.COMMAND_FAILED.format(fqdn, ex), fg='red')
-        click.secho(messages.COMMAND_FAILED_DETAIL.format(config.CLI_LOG_OUTPUT_PATH), fg='red')
-        # logger.error(ex, exc_info=True)
-
 def main():
-    """Main function for CLI.
-    """
-    print("weko-group-cache-db CLI")
+    """Run the command line interface for weko-group-cache-db."""
+    logger.info("weko-group-cache-db CLI")
+
+
+@main.command()
+@click.option(
+    "--file-path",
+    "-f",
+    type=click.Path(exists=True, dir_okay=False, path_type=str),
+    required=False,
+    default="institutions.toml",
+    help="Specify the path to the TOML file containing institution data.",
+)
+def run(file_path: str):
+    """Fetch and cache groups for all institutions."""
+    fetch_all(file_path)
